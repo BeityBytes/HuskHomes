03aeef8d87acded2ecdfac6d36f289c626446993
BeityBytes 🌸 <195160182+BeityBytes@users.noreply.github.com>
2025-12-02 22:38:56 +0100

    fix: integrate distance costs into Teleport system
    Update Teleport class to use confirmation prompts and distance-based costs:
    - Add getTeleportConfirmations() method to HuskHomes interface
    - Fix validation methods to pass positions properly
    - Handle inter-dimensional teleports correctly
    - Still need to fix NPE when user has no position
    - TODO: add economy integration for confirmations

---
+++ common/src/main/java/net/william278/huskhomes/command/ConfirmCommand.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of HuskHomes, licensed under the Apache License 2.0.
+ *
+ *  Copyright (c) William278 <will27528@gmail.com>
+ *  Copyright (c) contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.william278.huskhomes.command;
+
+import net.william278.huskhomes.HuskHomes;
+import net.william278.huskhomes.user.CommandUser;
+import net.william278.huskhomes.user.OnlineUser;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ConfirmCommand extends Command {
+
+    public ConfirmCommand(@NotNull HuskHomes plugin) {
+        super(List.of("confirm"), "", plugin);
+    }
+
+    @Override
+    public void execute(@NotNull CommandUser executor, @NotNull String[] args) {
+        if (!(executor instanceof OnlineUser user)) {
+            plugin.getLocales().getLocale("error_in_game_command")
+                    .ifPresent(executor::sendMessage);
+            return;
+        }
+
+        // Check if user has a pending teleport confirmation
+        plugin.getTeleportConfirmations().ifPresentOrElse(confirmations -> {
+            if (confirmations.confirmTeleport(user)) {
+                return;
+            }
+
+            // No pending confirmation
+            plugin.getLocales().getLocale("error_no_pending_teleport")
+                    .ifPresent(user::sendMessage);
+        }, () -> {
+            // Teleport confirmations not enabled
+            plugin.getLocales().getLocale("error_teleport_confirmations_disabled")
+                    .ifPresent(user::sendMessage);
+        });
+    }
+}
